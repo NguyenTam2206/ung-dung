8a582374f58eb0bd259f2328c659c3cfb75c81c7
Nguyen Minh <nguyenthientam.2206@gmail.com>
2019-10-28 19:58:49 +0700

    Updates

---
+++ precache-manifest.6654f4701402e47b9bf5dd52d3f3252a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119001571754acf731f7fd741f174a8c",
+    "url": "/ung-dung/index.html"
+  },
+  {
+    "revision": "dbf7a5570a574e079b8b",
+    "url": "/ung-dung/static/css/2.65222a67.chunk.css"
+  },
+  {
+    "revision": "4bfe798f703d68c533eb",
+    "url": "/ung-dung/static/css/main.8c44521a.chunk.css"
+  },
+  {
+    "revision": "dbf7a5570a574e079b8b",
+    "url": "/ung-dung/static/js/2.8795ba33.chunk.js"
+  },
+  {
+    "revision": "4bfe798f703d68c533eb",
+    "url": "/ung-dung/static/js/main.158a5366.chunk.js"
+  },
+  {
+    "revision": "0cf184d8de9e667ead20",
+    "url": "/ung-dung/static/js/runtime-main.528eb22d.js"
+  },
+  {
+    "revision": "1216d85770181a4d82f29aabc3d732e7",
+    "url": "/ung-dung/static/media/calbg.1216d857.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ung-dung/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ung-dung/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ung-dung/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ung-dung/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ung-dung/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
