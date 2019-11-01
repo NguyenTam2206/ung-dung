c06a6c45c002d913b3f056532066a545689375a2
Nguyen Minh <nguyenthientam.2206@gmail.com>
2019-11-01 23:02:44 +0700

    form validation

--- src/components/QLpage/Form/Form.js
+++ src/components/QLpage/Form/Form.js
@@ -10,7 +10,17 @@ const Form = (props) => {
         txtUser : '',
         txtPass : '',
         sltLevel : 'Thành viên',
+        notice : false
     })
+
+    const showNotice = () => {
+        if(state.notice){
+            return ( <div className="col-lg-12
+                                     col-md-12 thongbaothanhcong">
+                         <b>Đăng kí thành công</b>
+                     </div>)
+        }
+    }
 
     const changeInput = (event) => {
         const name = event.target.name;
@@ -30,16 +40,23 @@ const Form = (props) => {
         item.password = state.txtPass;
         item.level = state.sltLevel === 'Thành viên' ? 2 : 1;
         props.add(item);
+        setState({
+            ...state,
+            notice : true 
+        });
+        setInterval(() => {
+            setState({
+                ...state,
+                notice : false 
+            })
+        }, 2000);
     }
     return (
         <div>
             <div className="col-lg-4 visible-lg
                             col-md-4 visible-md" style={{marginTop: '60px'}}>
                 <div className="row">
-                    <div className="col-lg-12
-                                    col-md-12 thongbaothanhcong">
-                        <b>Đăng kí thành công</b>
-                    </div>
+                    { showNotice() }
                     <div className="col-lg-12
                                     col-md-12 thembox">
                         <div className="row">
@@ -54,9 +71,9 @@ const Form = (props) => {
                                             col-md-12">
                                 <form method="POST" onSubmit={ (e) => submitForm(e) }>
                                     <span style={{fontSize: '18px'}}>Thành viên</span><br></br>
-                                    <input type="text" name="txtUser" onChange={ (e) => changeInput(e)} placeholder="Nhập ID thành viên"  className="input" required minlength="5"></input><br></br>
+                                    <input type="text" name="txtUser" onChange={ (e) => changeInput(e)} placeholder="Nhập ID thành viên"  className="input" required minLength="5"></input><br></br>
                                     <br></br><span style={{fontSize: '18px'}}>Mật khẩu</span><br></br>
-                                    <input type="password" name="txtPass" onChange={ (e) => changeInput(e)} placeholder="Nhập mật khẩu" className="input" required minlength="5"></input><br></br>
+                                    <input type="password" name="txtPass" onChange={ (e) => changeInput(e)} placeholder="Nhập mật khẩu" className="input" required minLength="5"></input><br></br>
                                     <br></br><span style={{fontSize: '18px'}}>Quyền</span><br></br>
                                     <select name="sltLevel" className="input" onChange={ (e) => changeInput(e)} value={ state.sltLevel }>
                                         <option value="Thành viên">Thành viên</option>
